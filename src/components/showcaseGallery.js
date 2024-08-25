a260ecc16d641bbcf16fb9cd4e218f45eb486f95
TheR16H <rashawn5hall@gmail.com>
2024-08-25 10:55:08 -0400

    had to usestate to fix stuff. hooks are amazing

--- src/components/showcaseGallery.js
+++ src/components/showcaseGallery.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function importAllImages(r) {
-  let images = {};
-  r.keys().forEach((key) => {
-    images[key] = r(key);
-  });
-  return images;
-}
+const ShowcaseGallery = () => {
+  const [images, setImages] = useState([]);
 
-const images = importAllImages(require.context('../images', false, /\.(png|jpe?g|gif|svg)$/));
+  useEffect(() => {
+    const importAllImages = async () => {
+      const imageContext = require.context('../images', false, /\.(png|jpe?g|gif|svg)$/);
+      const keys = imageContext.keys();
+      const imagesArray = await Promise.all(keys.map(async (key) => {
+        const module = await imageContext(key);
+        return { key, src: module.default };
+      });
+      setImages(imagesArray);
+    };
 
-const showcaseGallery = () => {
+    importAllImages();
+  }, []);
+
   return (
     <div>
-      <h1>showcase</h1>
-      {Object.keys(images).map((key) => (
-        <img key={key} src={images[key].default} alt={key} />
+      <h1>Showcase</h1>
+      {images.map((image) => (
+        <img key={image.key} src={image.src} alt={image.key} />
       ))}
     </div>
   );
-}
+};
+
+export default ShowcaseGallery;
