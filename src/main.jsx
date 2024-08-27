2d202ab82ac2859e2ccb80c8f0bb8097b5695410
TheR16H <rashawn5hall@gmail.com>
2024-08-27 15:18:01 -0400

    got images working

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById('root');
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention:"camelCase",
-    },
-  },
   server: {
     port: 3000,
     open: true
