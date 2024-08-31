5d887f5387be25c269f2ff7fac862fc14af30ae0
TheR16H <rashawn5hall@gmail.com>
2024-08-30 22:43:58 -0400

    testing some changes

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 import styles from "./Experience.module.css";
 import skills from "../../data/skills.json";
+import history from "../../data/history.json";
 import { getImageUrl } from "../../utils";
 
 export const Experience = () => {
   return (
     <section className={styles.container} id="experience">
-      <h2 className={styles.title}>Experiencesss</h2>
+      <h2 className={styles.title}>I use these</h2>
       <div className={styles.content}>
         <div className={styles.skills}>
           {skills.map((skill, id) => {
