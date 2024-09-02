b088a5a22036b28da27ad23d361e5e0c170a4849
TheR16H <rashawn5hall@gmail.com>
2024-09-02 02:13:43 -0400

    changing.css

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,27 +1,48 @@
 import React from "react";
 
-import styles from "./exp.css";
+import "./exp.css";
 import skills from "../../data/skills.json";
 import { getImageUrl } from "../../utils";
 
 export const Experience = () => {
-  return (
-    <section className={styles.container} id="experience">
-      <h2 className={styles.title}>well versed</h2>
-      <div className={styles.content}>
-        <div className={styles.skills}>
-          {skills.map((skill, id) => {
-            return (
-              <div key={id} className={styles.skill}>
-                <div className={styles.skillImageContainer}>
-                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
-                </div>
-                <p>{skill.title}</p>
+//   return (
+//     <section className={styles.container} id="experience">
+//       <h2 className={styles.title}>well versed</h2>
+//       <div className={styles.content}>
+//         <div className={styles.skills}>
+//           {skills.map((skill, id) => {
+//             return (
+//               <div key={id} className={styles.skill}>
+//                 <div className={styles.skillImageContainer}>
+//                   <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+//                 </div>
+//                 <p>{skill.title}</p>
+//               </div>
+//             );
+//           })}
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+return (
+  <section className="container" id="experience">
+    <h2 className="title">well versed</h2>
+    <div className="content">
+      <div className="skills">
+        {skills.map((skill, id) => {
+          return (
+            <div key={id} className="skill">
+              <div className="skillImageContainer">
+                <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
               </div>
-            );
-          })}
-        </div>
+              <p>{skill.title}</p>
+            </div>
+          );
+        })}
       </div>
-    </section>
-  );
+    </div>
+  </section>
+);
 };
