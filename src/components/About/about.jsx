3574d90ca8d82de58bae7ba199539c9b016ece76
TheR16H <rashawn5hall@gmail.com>
2024-09-13 14:50:22 -0400

    got visuals

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -9,39 +9,38 @@ export const About = () => {
       <h2 className={styles.title}>About Me</h2>
       <div className={styles.content}>
         <img
-          src={getImageUrl("about/1-50x50.png")}
+          src={getImageUrl("about/3.png")}
           alt="Flower"
           className={styles.aboutImage}
         />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/1-59x50.png")} alt="Cursor icon" />
+            <img src={getImageUrl("about/1-50x50.png")} alt="CPU" className={styles.aboutImage} />
             <div className={styles.aboutItemText}>
-              <h3> Fullstack Developer</h3>
+              <h3>somethin</h3>
               <p>
-                I'm an aspiring fullstack developer with experience in building responsive
+             words, words, words 
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/4.png")} alt="MAIN" className={styles.aboutImage} />
+            <div className={styles.aboutItemText}>
+              <h3>Fullstack Developer</h3>
+              <p>
+              I'm an aspiring fullstack developer with experience in building responsive
                 and optimized sites, as well as developing fast and optimised back-end systems
                 and utlizing APIs. 
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/dbhb.png")} alt="Server icon" />
+            <img src={getImageUrl("about/2-50x50.png")} alt="Side"className={styles.aboutImage} />
             <div className={styles.aboutItemText}>
-              <h3>Passion Projects.</h3>
+              <h3>PASS PROJEC</h3>
               <p>
               in my freetime i tend to dive into various topics of research always advancing my extensive knowlege on the subject 
-             of fullstack and have a few passion projects planned to be released soon!
-              </p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/dkdk.png")} alt="UI icon" />
-            <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
-              <p>
-                I have designed multiple landing pages and have created design
-                systems as well
+              of fullstack and have a few passion projects planned to be released soon!
               </p>
             </div>
           </li>
