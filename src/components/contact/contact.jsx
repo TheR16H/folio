aa59a04cdfeeec5cae90511e1c449a359e0ab3eb
TheR16H <rashawn5hall@gmail.com>
2024-10-09 14:51:18 -0400

    Removed unneeded code

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -7,7 +7,6 @@ export const Contact = () => {
         <footer id="contact" className={styles.container}>
             <div className={styles.text}>
                 <h2>Contact me</h2>
-                {/* <p>hi </p> */}
             </div>
             <ul className={styles.links}>
         <li className={styles.link}>
