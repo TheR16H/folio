f84d64813fb83ed0cb7080d6208378ad558d812f
TheR16H <rashawn5hall@gmail.com>
2024-10-22 21:18:25 -0400

    updated skill

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,6 +1,6 @@
 const quotes = [
   "This is just the beginning.",
-  "A moment of quiet is good for your mental well-being. - Uncle Iroh",
+  "A moment of quiet is good for your mental wellbeing. - Uncle Iroh",
   "I wanna create fascinating things that make people say ''This is so cool!'' - Rashawn",
   "One of my most productive days was throwing away 1000 lines of code. - Ken Thompson",
   "K.I.S.S - Keep it simple, stupid."
