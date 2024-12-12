2056b5e7b2bfeb4720984e17ebc91b23d05d74a5
TheR16H <rashawn5hall@gmail.com>
2024-12-12 18:46:10 -0500

    added another Q

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,8 +1,9 @@
 const quotes = [
   "This is just the beginning.",
-  "A moment of quiet is good for your mental wellbeing. - Uncle Iroh",
+  "'A moment of quiet is good for your mental wellbeing.' - Uncle Iroh",
+  "'Never trust a computer you can't throw out a window' - Steve Wozniak",
   "I wanna create fascinating things that make people say ''This is so cool!'' - Rashawn",
-  "One of my most productive days was throwing away 1000 lines of code. - Ken Thompson",
+  "'One of my most productive days was throwing away 1000 lines of code.' - Ken Thompson",
   "K.I.S.S - Keep it simple, stupid."
 ];
 
