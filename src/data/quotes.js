9b8216c60ff167fb6d9f52e3f3bcc2d011801570
TheR16H <rashawn5hall@gmail.com>
2025-02-16 22:35:43 -0500

    added1mORE

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -4,7 +4,8 @@ const quotes = [
   "'Never trust a computer you can't throw out a window' - Steve Wozniak",
   "I wanna create fascinating things that make people say ''This is so cool!'' - Rashawn",
   "'One of my most productive days was throwing away 1000 lines of code.' - Ken Thompson",
-  "K.I.S.S - Keep it simple, stupid."
+  "K.I.S.S - Keep it simple, stupid.", 
+  "'Keep the main thing the main thing' - Stephen Covey"
 ];
 
 export default quotes;
