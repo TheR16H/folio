939bc174ab4e1f718ca5d3002955166b7255d94c
TheR16H <161267103+TheR16H@users.noreply.github.com>
2025-02-22 00:31:41 -0500

    Update quotes.js

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -5,7 +5,7 @@ const quotes = [
   "'I wanna create fascinating things that make people say ''This is so cool!''' - Rashawn",
   "'One of my most productive days was throwing away 1000 lines of code.' - Ken Thompson",
   "K.I.S.S - Keep it simple, stupid.", 
-  "'Keep the main thing the main thing' - Stephen Covey"
+  "'Keep the main thing, the main thing' - Stephen Covey"
 ];
 
 export default quotes;
